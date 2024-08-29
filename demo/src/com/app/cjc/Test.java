6792bba912326c34f8b9ae576083d4bfd693fcf9
PratikMane07 <168271590+PratikMane07@users.noreply.github.com>
2024-08-29 15:30:23 +0530

    add m2 method by sk

--- demo/src/com/app/cjc/Test.java
+++ demo/src/com/app/cjc/Test.java
@@ -6,6 +6,11 @@ public class Test {
 	{
 		System.out.println("This is m1 --> method");
 	}
+
+	public void m2()
+	{
+		System.out.println("This is m2 --> method");
+	}
 	
 	public static void main(String[] args) {
 		
